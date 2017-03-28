9f6b7d60c5de517152d9e89985f869fbdbf6be21
Saren Arterius <saren@wtako.net>
2017-03-29 01:15:26 +0800

    more human readable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from datetime import timedelta
+from math import floor
 from sys import argv
 import bwa
-from bwa import BWA
 
 if __name__ == '__main__':
     b = bwa.BWA(__file__)
@@ -11,4 +12,4 @@ if __name__ == '__main__':
         results = b.find_anime(argv[2])
         for result in results[:5]:
             print("{}%: '{}' | {}s".format(round((1 - result['val'] / bwa.FIND_BOUND) * 10000) / 100, result[
-                  'id'], round(result['position_second'] * 100) / 100))
+                  'id'], timedelta(seconds=floor(result['position_second']))))
